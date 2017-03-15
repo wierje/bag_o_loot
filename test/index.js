4bade0b4c6c0ad256648031f3327a10ab9fab43e
wierje <wier.je@gmail.com>
2017-03-15 10:50:06 -0500

    boilerplate created

---
+++ test/index.js
@@ -0,0 +1,8 @@
+import assert from 'assert';
+import bagOLoot from '../lib';
+
+describe('bag-o-loot', function () {
+  it('should have unit test!', function () {
+    assert(false, 'we expected this package author to add actual unit tests.');
+  });
+});
